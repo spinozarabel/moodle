d50a22e2dc042946cb39f2fef0d68211c56c096d
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2019-04-14 15:58:41 -0700

    Update view.php

--- mod/data/view.php
+++ mod/data/view.php
@@ -402,7 +402,7 @@ if ($showactivity) {
     /// Check the number of entries required against the number of entries already made (doesn't apply to teachers)
         if ($data->entriesleft = data_get_entries_left_to_add($data, $numentries, $canmanageentries)) {
             $strentrieslefttoadd = get_string('entrieslefttoadd', 'data', $data);
-            echo $OUTPUT->notification($strentrieslefttoadd);
+//MA            echo $OUTPUT->notification($strentrieslefttoadd);
         }
 
     /// Check the number of entries required before to view other participant's entries against the number of entries already made (doesn't apply to teachers)
