6f89b4a0eb8a8566a49d146a8e0660556cf7cec7
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2020-07-13 11:34:04 -0700

    Update core_renderer.php

--- theme/boost/classes/output/core_renderer.php
+++ theme/boost/classes/output/core_renderer.php
@@ -96,7 +96,7 @@ class core_renderer extends \core_renderer {
         // stdeunt in course page
         if (isloggedin()  &&  ($COURSE->id != SITEID))
         {
-            $context = context_course::instance($COURSE->id);
+            //$context = context_course::instance($COURSE->id);
             $is_student = has_capability('mod/assign:submit', 50);
             // add menu item called ThisCourse
             $course_id = $COURSE->id;
