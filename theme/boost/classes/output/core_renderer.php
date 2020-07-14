5134eb4205f352a81d494ca0ff7f0e27eaeeb915
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2020-07-14 09:57:49 -0700

    Update core_renderer.php

--- theme/boost/classes/output/core_renderer.php
+++ theme/boost/classes/output/core_renderer.php
@@ -129,10 +129,10 @@ class core_renderer extends \core_renderer {
                 foreach($sections as $idx => $section)
                 {
                     // add a menu item for each section in the loop
-                    $section_num    += 1; // increment section number, 1st section starts with 1
                     $section_name   = get_section_name($COURSE, $section);
                     $section_url    = new moodle_url('/course/view.php' . '?id=' . $course_id . '#section-' . $section_num);
                     $branch_sections->add($section_name, $section_url, $section_name);
+                    $section_num    += 1; // increment section number, 1st section starts with 0
                 }
             }
 
