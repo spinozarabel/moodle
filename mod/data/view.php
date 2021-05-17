72a07c3e095f7bc0b909cb6f447027b24386d3ee
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2021-05-17 09:58:56 +0530

    made edits to mod/data/view.php

--- mod/data/view.php
+++ mod/data/view.php
@@ -401,14 +401,24 @@ if ($showactivity) {
     /// Check the number of entries required against the number of entries already made (doesn't apply to teachers)
         if ($data->entriesleft = data_get_entries_left_to_add($data, $numentries, $canmanageentries)) {
             $strentrieslefttoadd = get_string('entrieslefttoadd', 'data', $data);
-            echo $OUTPUT->notification($strentrieslefttoadd);
+
+            // Madhu added customization echo only when name of activity does not contain 'leave management system'
+			if (stripos($data->name, 'leave management system') === false) 
+            {
+                echo $OUTPUT->notification($strentrieslefttoadd);
+            }
         }
 
     /// Check the number of entries required before to view other participant's entries against the number of entries already made (doesn't apply to teachers)
         $requiredentries_allowed = true;
         if ($data->entrieslefttoview = data_get_entries_left_to_view($data, $numentries, $canmanageentries)) {
             $strentrieslefttoaddtoview = get_string('entrieslefttoaddtoview', 'data', $data);
-            echo $OUTPUT->notification($strentrieslefttoaddtoview);
+
+            // Madhu added customization echo only when name of activity does not contain 'leave management system'
+			if (stripos($data->name, 'leave management system') === false)
+            {
+                echo $OUTPUT->notification($strentrieslefttoaddtoview);
+            }
             $requiredentries_allowed = false;
         }
 
